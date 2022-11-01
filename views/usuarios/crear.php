bdcf8d08214257c372907116d2c8cb03146bface
civic100 <114212306+civic100@users.noreply.github.com>
2022-11-01 16:47:42 +0100

    Delete views directory

--- views/usuarios/crear.php
+++
@@ -1,7 +0,0 @@
-<form action="index.php?controller=Usuario&action=alta" method="post">
-    Nombre:
-    <input type="text" name = "nombre">
-    Contraseña:
-    <input type="password" name = "pass">    
-    <input type = "submit" value="Alta">
-</form>
