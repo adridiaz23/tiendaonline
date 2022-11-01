ec5e2d833add15c267d8f78f6614f3de021f0920
civic100 <114212306+civic100@users.noreply.github.com>
2022-11-01 16:47:13 +0100

    Delete controllers directory

--- controllers/BaseController.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    class Sesion{
-        public static function destruir(){
-            session_start();
-            session_unset();
-            session_destroy();
-        }
-    }
-?>
