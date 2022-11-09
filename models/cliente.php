ce4603f285d7b2327a4bd1f3b4e89302322cc847
civic100 <114212306+civic100@users.noreply.github.com>
2022-11-09 09:14:14 +0100

    Admin-Cliente

--- models/cliente.php
+++ models/cliente.php
@@ -13,6 +13,76 @@
         private $codigoPostal;
         
         //Generamos los set y get.
-
-
+        public function getNombre()
+        {
+                return $this->nombre;
+        }
+        public function setNombre($nombre)
+        {
+                $this->nombre = $nombre;
+                return $this;
+        }
+        public function getApellido()
+        {
+                return $this->apellido;
+        }
+        public function setApellido($apellido)
+        {
+                $this->apellido = $apellido;
+                return $this;
+        }
+        public function getCorreoCliente()
+        {
+                return $this->correoCliente;
+        }
+        public function setCorreoCliente($correoCliente)
+        {
+                $this->correoCliente = $correoCliente;
+                return $this;
+        }
+        public function getCalle()
+        {
+                return $this->calle;
+        }
+        public function setCalle($calle)
+        {
+                $this->calle = $calle;
+                return $this;
+        }
+        public function getNumero()
+        {
+                return $this->numero;
+        }
+        public function setNumero($numero)
+        {
+                $this->numero = $numero;
+                return $this;
+        }
+        public function getDni()
+        {
+                return $this->dni;
+        }
+        public function setDni($dni)
+        {
+                $this->dni = $dni;
+                return $this;
+        }
+        public function getPassword()
+        {
+                return $this->password;
+        } 
+        public function setPassword($password)
+        {
+                $this->password = $password;
+                return $this;
+        }
+        public function getCodigoPostal()
+        {
+                return $this->codigoPostal;
+        }
+        public function setCodigoPostal($codigoPostal)
+        {
+                $this->codigoPostal = $codigoPostal;
+                return $this;
+        }
     }
