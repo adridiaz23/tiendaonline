3925fd436e45bfa3e18aee1b540bb2669d59b702
davidFeid <davidbuny@gmail.com>
2022-11-30 09:24:13 +0100

    commit lol

--- views/producto/buscador.php
+++ views/producto/buscador.php
@@ -3,13 +3,15 @@
         echo '<h2>Resultados encontrados de "'.$_POST['buscador'].'"</h2>';
     echo "</div>";
     echo "<div class='containerBuscador'>";
-        echo "<div class='productoBuscador'>";
+    foreach($listadoProductos as $clave =>$valor){
+        echo "<div class='productoBuscador".(($clave+1)%4)."'>";
             echo "<div class='imagenBuscador'>";
-                echo "<img src='".$listadoProductos[0]->imagen."'>";
+                echo "<a href='index.php?controller=Producto&action=paginaProducto&isbn=".$valor->ISBN."'><img src='".$valor->imagen."'></img></a>";
             echo "</div>";
             echo "<div class='textoBuscador'>";
-                echo "<p>".$listadoProductos[0]->nombre."</p>";
+                echo "<p><a href='index.php?controller=Producto&action=paginaProducto&isbn=".$valor->ISBN."'>".$valor->nombre."</a></p>";
             echo "</div>";
         echo "</div>";
-    echo "</div>";
+    }
+    echo "</div>";   
 ?>
