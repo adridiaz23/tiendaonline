7ed402ed439498a33918ff3721a4b26be9f31167
civic100 <114212306+civic100@users.noreply.github.com>
2023-01-08 17:01:04 +0100

    a

--- views/general/contacto.php
+++ views/general/contacto.php
@@ -1,13 +1,25 @@
 <div class="contactMap">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.390659115624!2d2.237347614929413!3d41.45244159992351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4bb7236bf4449%3A0xc6b13e5d825a85a7!2sInstitut%20La%20Pineda!5e0!3m2!1sen!2ses!4v1669328487637!5m2!1sen!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.390659115624!2d2.237347614929413!3d41.45244159992351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4bb7236bf4449%3A0xc6b13e5d825a85a7!2sInstitut%20La%20Pineda!5e0!3m2!1sen!2ses!4v1669328487637!5m2!1sen!2ses" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
+<div class="cajaInfoContacto">
+    <div class="contactInfo">
+        Telefono: 645242487
+    </div>
 
-<div class="contactInfo">
-    Telefono: 645242487
+    <div class="contactInfo">
+        Correo de contacto: TiendaOnline@gail.com
     </div>
-<div class="contactInfo">
-    Correo de contacto: contact@gail.com
+
+    <div class="contactInfo">
+        Dirección: Institut La Pineda, Badalona(08910), España
     </div>
-<div class="contactInfo">
-    Dirección: Institut La Pineda, Badalona(08910), España
+
+    <div class="fromGenerico">
+                
+        <label for="nombre">Usuario:</label>
+        <input type="text"  placeholder="Usuario" name="nombre" id="nombre" autofocus>
+
+        <label for="Telefono">Telefono:</label>
+        <input type="text" placeholder="Telefono" name="Telefono" id="Telefono">
+    </div>
 </div>
